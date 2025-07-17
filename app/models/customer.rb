0419052f92c067f2c2b6cd987b19c5dc3088e4a5
EC2 Default User <ec2-user@ip-172-31-2-236.ap-northeast-1.compute.internal>
2025-07-17 11:45:39 +0000

    add model customers and address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :address, dependent: :destroy
+  has_many :cart_item, dependent: :destroy
+  has_many :order, dependent: :destroy
 end
