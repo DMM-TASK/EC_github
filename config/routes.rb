b519cced40a99b6923dd0bc01fc082b643eba34c
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-07-18 04:53:28 +0000

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+ 
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
   devise_for :admins
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
