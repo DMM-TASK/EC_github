fc530b46082545139d99a2e129ba99afa161fea1
KJ-del-lgtm <210510528+KJ-del-lgtm@users.noreply.github.com>
2025-07-19 13:11:40 +0900

    Update customers_controller.rb
    add private fm:customers.controller.rb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,7 +13,7 @@ class CustomersController < ApplicationController
     redirect_to customers_my_page_path(customer.id)
   end
 
-
+private
   def customer_params
     params.require(:customer).permit(:last_name,:first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :telephone_number, :is_active,:email)
   end
