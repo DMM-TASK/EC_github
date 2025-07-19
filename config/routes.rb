4557abadb2a5b9701dcafe291d0adaa882b7f274
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2025-07-19 05:08:34 +0000

    会員画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   devise_for :admins
   devise_for :users
+  namespace :adomin do
+    resources :customer, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
