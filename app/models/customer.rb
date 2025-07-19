a3f07f30d34583da334800c7ba86db2fbea571ac
EC2 Default User <ec2-user@ip-172-31-2-236.ap-northeast-1.compute.internal>
2025-07-19 03:58:11 +0000

    モデル修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,10 +9,10 @@ class Customer < ApplicationRecord
   has_many :order, dependent: :destroy
 
   def full_name
-    last_name + '' + first_name
+    first_name + " " + last_name
   end
 
   def full_name_kana
-    last_name_kana + '' + first_name_kana
+    first_name_kana + " " + last_name_kana
   end
 end
