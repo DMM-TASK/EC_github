35b4eb0f4f6bfeb87abe0b9c7b7c2770d2868e96
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2025-07-21 04:38:49 +0000

    コントローラとアクションを追加

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,23 @@
+class Admin::OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @customer = @order.customer
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order)
+    else
+      render 'show'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
+  end
+  
   devise_for :customers
   root to: "homes#top"
   get "/about" => "homes#about"
