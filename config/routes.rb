e7c18393e072b413d37508b4f7a12e8ab48d3ba3
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-07-22 06:40:08 +0000

    admin-signup

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,7 @@ Rails.application.routes.draw do
 
   devise_for :customers
 
-  devise_for :admins
-  root to: "homes#top"
+  
 
   resources :items, only: [:show, :index] 
   root  to: "homes#top"
