dca9c7b220c14c4c9e754b14d18c987e0ad80a80
EC2 Default User <ec2-user@ip-172-31-36-49.ap-northeast-1.compute.internal>
2025-07-23 03:58:57 +0000

    admin/customeer再度追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       admin_root_path
@@ -8,15 +8,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
-    case resource
-    when Admin
-      admin_root_path 
-    when Customer
-      public_root_path 
-    else
-      root_path
-    end
-  end
+  
+  
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
@@ -28,6 +21,8 @@ class ApplicationController < ActionController::Base
 
 
   protected
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name,:last_name])
+  end
 end
-
