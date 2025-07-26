755a5a08ac7638fc7e7ed03f33e5ea91fb5742e1
mikiodesu <212180654+mikiodesu@users.noreply.github.com>
2025-07-26 04:45:14 +0000

    before merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   namespace :admin do
     root to: 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
@@ -13,63 +14,52 @@ Rails.application.routes.draw do
 
   scope module: :public do
 
-
-    root to: 'homes#top'
-    
-
+    root to: "homes#top"
     resources :items, only: [:index, :show]
 
+    get "/about" => "homes#about"
+
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
     resources :orders, only: [:new, :create, :index, :show] do
       collection do
-        post 'confirm'  
-        get 'thanks'  
+        post 'confirm'
+        get 'thanks'
       end
     end 
-
-  end
-  
-  resources :cart_items, only: [:index, :update, :destroy, :create] do
-    collection do
-      delete 'destroy_all'
-  
     resources :cart_items, only: [:index, :update, :destroy, :create] do
       collection do
         delete 'destroy_all'
       end
     end
-  end
 
 
-  get 'cart_items', to: 'cart_items#index'
-  patch 'cart_items/:id', to: 'cart_items#update'
-  delete 'cart_items/:id', to: 'cart_items#destroy'
-  delete 'cart_items/destroy_all', to: 'cart_items#destroy_all'
-  post 'cart_items', to: 'cart_items#create'
+    get 'cart_items', to: 'cart_items#index'
+    patch 'cart_items/:id', to: 'cart_items#update'
+    delete 'cart_items/:id', to: 'cart_items#destroy'
+    delete 'cart_items/destroy_all', to: 'cart_items#destroy_all'
+    post 'cart_items', to: 'cart_items#create'
   
 
     get "/about" => "homes#about"
-
-    get "customers/my_page/:id" => "customers#show", as: 'customer'
-    get "customers/information/:id/edit" => "customers#edit", as: 'edit_customer'
-    patch "customers/my_page/:id" => "customers#update", as: 'update_customer'
-    get "/customers/unsubscribe" => "customers#unsubscribe", as: 'unsubscribe_customer'
-    patch "/customers/withdraw" => "customers#withdraw", as: 'withdraw_customer'
-
-  end
   
 
+  end
+
   devise_for :customers, controllers: {
-  registrations: "publics/registrations",
-  sessions: 'publics/sessions',
-  passwords: 'publics/passwords'
+    registrations: "publics/registrations",
+    sessions: 'publics/sessions',
+    passwords: 'publics/passwords'
   }
 
   devise_for :admins, controllers: {
+
   sessions: 'admins/sessions',
   registrations: 'admins/registrations',
   passwords: 'admins/passwords'
+
   }
+
 
   get "/about" => "public/homes#about"
   get "customers/my_page/:id" => "public/customers#show", as: 'customer'
